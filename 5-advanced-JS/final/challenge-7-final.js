f8419af4a97d2e4f93fabe50de81a11bf518a1fc
Gonçalo Bernardo <40314663+ShakiBoy@users.noreply.github.com>
2020-03-26 15:05:25 +0000

    final final challenge 7

--- 5-advanced-JS/final/challenge-7-final.js
+++ 5-advanced-JS/final/challenge-7-final.js
@@ -1,137 +1,196 @@
-(function(){/*****************************
- * SET PAGE ELEMENTS
-*/
+(function(){
 
-var submitButton = document.getElementById('submit-answer');
-var comboOptions = document.getElementById('answer');
-var restartButton = document.getElementById("restart-question");
-var resultLine = document.getElementById("result");
+    var gameManager;
 
-var manager;
+    /*****************************
+     * CREATE THE CONSTRUCTOR QUESTION
+    */
+    var Question = function(question, questionOptions, correctOption) {
 
-/*****************************
- * SET SUBMIT AND COMBO DISABLED
-*/
-disableElements();
+        this.question = question;
+        this.questionOptions = questionOptions;
+        this.correctOption = correctOption;
+    };
 
-/*****************************
- * CREATE THE CONSTRUCTOR QUESTION
-*/
-var Question = function(question, questionOptions, correctOption) {
+    /*****************************
+     * CREATE THE CONSTRUCTOR MANAGER
+    */
+    var QuestionsManager = function() {
 
-    this.question = question;
-    this.questionOptions = questionOptions;
-    this.correctOption = correctOption;
-};
+        this.questionsList = [];
+        this.questionsList.push(new Question('What time is it?', ['It is 8am.', 'It is 10am.', 'It is 12pm.'], 1));
+        this.questionsList.push(new Question('What is my name?', ['Jorge.', 'Goncalo.', 'Simao.'], 1));
+        this.questionsList.push(new Question('How old am I?', ['22 years old.', '38 years old.', '30 years old.'], 2));
+        this.questionsList.push(new Question('What is my favorite color?', ['Green', 'Yellow.', 'Red.'], 0));
+        this.questionsList.push(new Question('What is my favorite movie?', ['Titanic.', 'Match Point.', 'Contratiempo'], 2));
+    };
 
-/*****************************
- * CREATE THE QUESTION METHODS
-*/
-Question.prototype.createComboBox = function(){
+    /*****************************
+     * CREATE THE CONSTRUCTOR GAME MANAGER
+    */
+    var GameManager = function() {
 
-    for (i = 0 ; i < this.questionOptions.length; i++){
-        opt = document.createElement("option");
-        opt.value = i;
-        opt.text = this.questionOptions[i];
-        comboOptions.add(opt, comboOptions.options[i]);
-    }
-};
+        this.manager = new QuestionsManager();
+        this.submitButton = document.getElementById('submit-answer');
+        this.comboOptions = document.getElementById('answer');
+        this.restartButton = document.getElementById("restart-question");
+        this.resultLine = document.getElementById("result");
+        this.quitButton = document.getElementById("quit");
+        this.pointsLine = document.getElementById("score");
+        this.questionString = document.querySelector('.question');
+        this.points = 0;
+        this.POINTS_RANGE = [-2, 2];
+    };
 
-/*****************************
- * CREATE THE CONSTRUCTOR MANAGER
-*/
-var QuestionsManager = function() {
+    /*****************************
+     * CREATE THE MANAGER METHODS
+    */
+    QuestionsManager.prototype.selectQuestion = function(){
 
-    this.questionsList = [];
-    this.questionsList.push(new Question('What time is it?', ['It is 8am.', 'It is 10am.', 'It is 12pm.'], 1));
-    this.questionsList.push(new Question('What is my name?', ['Jorge.', 'Goncalo.', 'Simao.'], 1));
-    this.questionsList.push(new Question('How old am I?', ['22 years old.', '38 years old.', '30 years old.'], 2));
-    this.questionsList.push(new Question('What is my favorite color?', ['Green', 'Yellow.', 'Red.'], 0));
-    this.questionsList.push(new Question('What is my favorite movie?', ['Titanic.', 'Match Point.', 'Contratiempo'], 2));
-};
+        var qNumber = Math.floor(Math.random() * this.questionsList.length);
+        this.currentQuestion = this.questionsList[qNumber];
+        gameManager.questionString.textContent = this.currentQuestion.question;
+    };
 
-/*****************************
- * CREATE THE MANAGER METHODS
-*/
-QuestionsManager.prototype.selectQuestion = function(){
+    /*****************************
+     * CREATE THE NEEDED MANAGER GETTERS
+    */
+    QuestionsManager.prototype.getCorrectOption = function(){
+        return this.currentQuestion.correctOption;
+    };
 
-    var qNumber = Math.floor(Math.random() * this.questionsList.length);
-    this.currentQuestion = this.questionsList[qNumber];
-    document.querySelector('.question').textContent = this.currentQuestion.question;
-};
+    /*****************************
+     * CREATE THE GAME MANAGER METHODS
+    */
+    GameManager.prototype.resetResultColor = function(){
 
-/*****************************
- * CREATE THE NEEDED MANAGER GETTERS
-*/
-QuestionsManager.prototype.getCorrectOption = function(){
-    return this.currentQuestion.correctOption;
-};
+        this.resultLine.classList.remove('waitAnswer');
+        this.resultLine.classList.remove('rightAnswer');
+        this.resultLine.classList.remove('wrongAnswer');
+    };
 
-/*****************************
- * EVENTS
-*/
-restartButton.addEventListener('click', function(){
+    GameManager.prototype.disableElements = function(){
 
-    for (i = 0 ; i < comboOptions.length; i++){
-        comboOptions.remove(i);
-    }
-    comboOptions.remove(0);
+        this.submitButton.disabled = true;
+        this.comboOptions.disabled = true;
+    };
 
-    manager = new QuestionsManager();
-    manager.selectQuestion();
-    manager.currentQuestion.createComboBox();
+    GameManager.prototype.nextQuestion = function(){
 
-    resetResultColor();
-    resultLine.textContent = 'Waiting for an answer...';
+        for (i = 0 ; i < this.comboOptions.length; i++){
+            this.comboOptions.remove(i);
+        }
+        this.comboOptions.remove(0);
 
-    submitButton.disabled = (comboOptions.length > 0) ? false : true;
-    comboOptions.disabled = (comboOptions.length > 0) ? false : true;
-});
+        this.manager.selectQuestion();
+        this.manager.currentQuestion.createComboBox();
 
-submitButton.addEventListener('click', function(){
-    
-    var currentAnswer = parseInt(comboOptions.value);
-    if (currentAnswer === manager.getCorrectOption()){
-        resetResultColor();
-        resultLine.classList.add('rightAnswer')
-        resultLine.textContent = 'Right!';
-        disableElements();
-    }else{
-        resetResultColor();
-        resultLine.classList.add('wrongAnswer')
-        resultLine.textContent = 'Wrong! Try again.';
-    }
-});
+        this.resetResultColor();
+        this.showResult('waitAnswer', 'Waiting for an answer...', false);
 
-comboOptions.addEventListener('click', function(){
-    resetResultColor();
-    resultLine.textContent = 'Waiting for an answer...';
-});
+        this.submitButton.disabled = (this.comboOptions.length > 0) ? false : true;
+        this.comboOptions.disabled = (this.comboOptions.length > 0) ? false : true;
+    };
 
-/*****************************
- * USEFUL FUNCTIONS
-*/
-function resetResultColor(){
+    GameManager.prototype.managePoints = function(win){
 
-    resultLine.classList.remove('rightAnswer');
-    resultLine.classList.remove('wrongAnswer');
-}
+        if (win){
+            this.points++;
+        }else{
+            this.points--
+        }
+        this.pointsLine.textContent = this.points;
+        if (this.points <= gameManager.POINTS_RANGE[0]){
+            this.disableElements();
+            this.quitButton.disabled = true;
+            this.showResult('wrongAnswer', 'YOU LOST THE GAME!', true);
+            this.pointsLine.textContent = 'Your final score is: ' + gameManager.points;
+        }
+        if (this.points >= gameManager.POINTS_RANGE[1]){
+            this.disableElements();
+            this.quitButton.disabled = true;
+            this.showResult('rightAnswer', 'YOU WON THE GAME!', true);
+            this.pointsLine.textContent = 'Your final score is: ' + gameManager.points;
+        }
+    };
 
-function disableElements(){
+    GameManager.prototype.showResult = function(answerClass, resultText, isFinal){
 
-    submitButton.disabled = true;
-    comboOptions.disabled = true;
-}
+        gameManager.resultLine.classList.add(answerClass)
+        gameManager.resultLine.textContent = resultText;
+        if (isFinal){
+            gameManager.questionString.textContent = '';
+        };
+    };
+
+    gameManager = new GameManager();
+
+    /*****************************
+     * SET SUBMIT AND COMBO DISABLED
+    */
+    gameManager.disableElements();
+    gameManager.quitButton.disabled = false;
+
+    /*****************************
+     * CREATE THE QUESTION METHODS
+    */
+    Question.prototype.createComboBox = function(){
+
+        for (i = 0 ; i < this.questionOptions.length; i++){
+            opt = document.createElement("option");
+            opt.value = i;
+            opt.text = this.questionOptions[i];
+            gameManager.comboOptions.add(opt, gameManager.comboOptions.options[i]);
+        }
+    };
+
+    /*****************************
+     * EVENTS
+    */
+    gameManager.restartButton.addEventListener('click', function(){
+        
+        gameManager.points = 0;
+        gameManager.pointsLine.textContent = 0;
+        gameManager.quitButton.disabled = false;
+        gameManager.resetResultColor();
+        gameManager.nextQuestion();
+    });
+
+    gameManager.submitButton.addEventListener('click', function(){
+        
+        var currentAnswer = parseInt(gameManager.comboOptions.value);
+        gameManager.resetResultColor();
+        if (currentAnswer === gameManager.manager.getCorrectOption()){
+            gameManager.showResult('rightAnswer', 'Right!', false);
+            gameManager.managePoints(true);
+        }else{
+            gameManager.showResult('wrongAnswer', 'Wrong!', false);
+            gameManager.managePoints(false);
+        }
+
+        if (gameManager.points > gameManager.POINTS_RANGE[0] && gameManager.points < gameManager.POINTS_RANGE[1]){
+            gameManager.disableElements();
+            gameManager.questionString.textContent = 'Loading next question...';
+            setTimeout(function() {gameManager.nextQuestion();}, 3000);
+        }
+        
+    });
+
+    gameManager.comboOptions.addEventListener('click', function(){
+
+        gameManager.resetResultColor();
+        gameManager.showResult('waitAnswer', 'Waiting for an answer...');
+    });
+
+    gameManager.quitButton.addEventListener('click', function(){
+
+        gameManager.resetResultColor();
+        gameManager.disableElements();
+        gameManager.points = 0;
+        gameManager.questionString.textContent = 'You quit the game...';
+        gameManager.pointsLine.textContent = 'Your final score is: ' + gameManager.points;
+        gameManager.quitButton.disabled = true;
+        gameManager.showResult('', '');
+    });
+
 })();
-
-/*
---- Expert level ---
-
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
-
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
-
-10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
-
-11. Display the score in the console. Use yet another method for this.
-*/
