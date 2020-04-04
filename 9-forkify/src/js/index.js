a006c4d57532ecb7904014e1b7d22877f8e65ead
Gonçalo Bernardo <40314663+ShakiBoy@users.noreply.github.com>
2020-04-04 18:56:52 +0100

    almost in the end... missing likes view controller and model and the final little things (including storage)

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,8 +1,14 @@
 import Search from './models/search';
+import Recipe from './models/recipe';
+import List from './models/list';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 const state = {};
+
+window.state = state;
 
 const controlSearch = async () => {
     // 1. Get the query from the view
@@ -15,12 +21,19 @@ const controlSearch = async () => {
         // 3. Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
-        // 4. Search for recipes
-        await state.search.getResults();
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
 
-        // 5. Render results on Ui
-        searchView.renderResults(state.search.result);
+            // 5. Render results on Ui
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
     }
 
 };
@@ -30,4 +43,122 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
-// recipe.js const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+elements.searchResPages.addEventListener('click', e => {
+
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+const controlRecipe = async () => {
+
+    const id = window.location.hash.replace('#', '');
+
+    if (id) {
+        // prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // highlightselected search item
+        if (state.search) {
+            searchView.highlightSelected(id);
+        };
+
+        // create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+
+            // get recipe data and parse ingredients
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            // calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // render the recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
+        }catch (err) {
+
+            alert('Error processing recipe!');
+            alert(err);
+        }
+        
+    }
+}
+
+//window.addEventListener('hashchange', controlRecipe);
+//window.addEventListener('load', controlRecipe);
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+const controlList = () => {
+
+    // create a new list if there not exists
+    if (!state.list) {
+
+        state.list = new List();
+    }
+
+    // add each ingredient to the list and user interface
+    state.recipe.ingredients.forEach(element => {
+
+        const item = state.list.addItem(element.count, element.unit, element.ingredient);
+        listView.renderItem(item);
+    });
+}
+
+// handling recipe and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // handle the delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        
+        // delete from state
+        state.list.deleteItem(id);
+
+        // delete from ui
+        listView.deleteItem(id);
+
+        // handle the count update
+    }else if (e.target.matches('.shopping__count--value')) {
+
+        const value = parseFloat(e.target.value);
+
+        if (value < 0 || isNaN(value)){
+            e.target.value = 0;
+        }
+        state.list.updateCount(id, value);
+        
+    }
+});
+
+// handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+
+        // decrease button is clicked
+        if (state.recipe.servings > 1){
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }  
+    }else if (e.target.matches('.btn-increase, .btn-increase *')) {
+
+        // decrease button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+
+        controlList();
+
+    }
+});
--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -8,6 +8,32 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
+}
+
+export const highlightSelected = id => {
+
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(element => {
+        element.classList.remove('results__link--active');
+    });
+    
+    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+
+}
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, current) => {
+            if (acc + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return acc + current.length
+        }, 0);
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
 }
 
 const renderRecipe = recipe => {
@@ -18,16 +44,55 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
     </li>`;
 
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
-}
+};
 
-export const renderResults = recipes => {
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page -1 : page + 1}>
+        <span>Page ${type === 'prev' ? page -1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>  
+    </ button>
+    `
+const renderButtons = (page, numResults, resPerPage) => {
 
-    recipes.forEach(renderRecipe);
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let button;
+    if (page === 1 && pages > 1) {
+        // Button to go to next page
+        button = createButton(page, 'next');
+    } else if (page === pages) {
+        // Only button to go to next page
+
+        button = createButton(page, 'prev');
+    }else{
+        // Both buttons
+        button = `
+        ${createButton(page, 'next')}
+        ${createButton(page, 'prev')}
+        `;
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+
+    // render the results of current page
+    const start = (page-1) * resPerPage;
+    const end = page * resPerPage;
+    
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // render the pagination buttons
+    renderButtons(page, recipes.length, resPerPage);
 }
